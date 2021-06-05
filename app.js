02840052c5bf8bae240edc00dd36b1abc9041ec3
venom666-del <71707267+venom666-del@users.noreply.github.com>
2021-06-05 23:32:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var PORT = process.env.PORT || 7004;
+
+// serve the web with files from '/public'
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('listening on port 3000');
+});
